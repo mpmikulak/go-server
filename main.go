8664d85e9935c9f2b3eca86910a540e2b69b0225
Mike MIkulak <mpmikulak@gmail.com>
2025-03-20 18:03:48 +0000

    Updates the create handler to reject a non POST http method

--- main.go
+++ main.go
@@ -18,10 +18,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet!"))
+	w.Write([]byte("Display a specific snippet!\n"))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet!"))
 }
 
