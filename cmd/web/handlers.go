0e6370cc2f0df33849b6cdbe92db1f1d15db0f96
Mike Mikulak <mpmikulak@gmail.com>
2025-03-27 20:14:14 -0500

    Adds html templates and partials

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,6 +18,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// file in the slice.
 	files := []string{
 		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
 		"./ui/html/pages/home.html",
 	}
 
@@ -30,7 +31,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
