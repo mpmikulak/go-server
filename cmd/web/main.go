65d606c3a02213ff48af081c20fcb7f6450a201d
Mike Mikulak <mpmikulak@gmail.com>
2025-04-07 18:30:29 -0500

    Adds chapter 3.1 Introduces an idiomatic way of using command line args

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-const serverPort = ":4000"
-
 func main() {
+	// Define a new command line flag with the name "addr", default value of ":4000"
+	// and some help text explaining what's what
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Call Parse() to bind the command line variable to the addr variable
+	// Must use parse prior to using the variable
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -17,7 +24,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", serverPort)
-	err := http.ListenAndServe(serverPort, mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
