d13ad7d9a36a46d1172179e01752fa1e7d89d5c9
Mike Mikulak <mpmikulak@gmail.com>
2025-04-07 18:55:33 -0500

    Adds part 3.2

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,16 +4,23 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Define a new command line flag with the name "addr", default value of ":4000"
-	// and some help text explaining what's what
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
-	// Call Parse() to bind the command line variable to the addr variable
-	// Must use parse prior to using the variable
 	flag.Parse()
+
+	// Use log.New() to create a logger for writing info messages
+	// Takes three parameters: destination to write logs to (os.Stdout), a string prefix (INFO and tab),
+	// and flags to indicate what additional info to include (local date and time).
+	// Flags are joined by bitwise OR operator |
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way but use stderr as the destination and use log.Lshortfile
+	// flag to include the relevant file name and line number
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -24,7 +31,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Initialize a new http.Server struct.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
